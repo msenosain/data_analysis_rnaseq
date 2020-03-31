844053d94b4c368865cb9dc52d6de97900e4b840
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-03-31 15:10:12 +0200

    first commit

---
+++ R/10_datacompilation.R
@@ -0,0 +1,97 @@
+library(edgeR)
+fc <- read.delim("counts_fcounts.txt", stringsAsFactors = FALSE)
+genes <- fc[,1:6]
+counts <- data.matrix(fc[,7:11])
+row.names(counts) <- paste(genes$Geneid, genes$Start, genes$End, sep=".")
+group <- factor( c("FL","FL","HL","HL") )
+y <- DGEList(counts, genes=genes, group=group)
+
+
+rnaseq_counts <- read.delim("~/Documents/Massion_lab/RNA Seq/RnaSeq_3388YZ_Tumor/RnaSeq_3388YZ_Tumor.count.txt")
+
+genes <- rnaseq_counts[,1:8]
+counts <- as.matrix(rnaseq_counts[,9:ncol(rnaseq_counts)])
+
+y <- DGEList(counts, genes=genes)
+
+# y$samples$group <- CANARY or any other classification
+
+# y$samples$lane <- lane  (batch)
+
+# y$samples should have all information on the samples that will be using in the analysis
+
+###############################################################################
+
+# First Batch
+rna_3388YZ_path <- "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/Vanderbilt/Massion_Lab/Projects/CyTOF_ADC_project/Data/RNA Seq/RnaSeq_3388YZ_Tumor/RnaSeq_3388YZ_Tumor.count.txt"
+
+rna_3388YZ <- read.delim(rna_3388YZ_path)
+
+p_3388YZ <- readxl::read_excel("Library/Mobile Documents/com~apple~CloudDocs/Documents/Vanderbilt/Massion_Lab/Projects/CyTOF_ADC_project/Data/RNA Seq/TMA 36 RNA DNA.xlsx", 
+    sheet = "3388-YZ", col_types = c("text", 
+        "text"))
+
+x <- match(paste0('pt.', p_3388YZ$pt_ID), colnames(rna_3388YZ)[9:ncol(rna_3388YZ)]) + 8
+
+rna_3388YZ <- rna_3388YZ[,c(1:8,x)]
+
+colnames(rna_3388YZ)[9:70] == paste0('pt.', p_3388YZ$pt_ID) # confirm
+
+colnames(rna_3388YZ)[9:70] = p_3388YZ$Vantage_ID
+
+p_3388YZ['Batch'] = 1
+
+rna_3388YZ$Feature_gene_name1 <- NULL
+
+p_3388YZ[37, 2] <- 14301
+p_3388YZ[56, 2] <- 8356
+
+
+# Second Batch
+
+rna_4163YZ_path <- "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/Vanderbilt/Massion_Lab/Projects/CyTOF_ADC_project/Data/RNA Seq/RnaSeq_4163YZ_Tumor/RnaSeq_3388YZ_Tumor/RnaSeq_3388YZ_Tumor.count.txt"
+
+rna_4163YZ <- read.delim(rna_4163YZ_path)
+
+p_4163YZ <- readxl::read_excel("Library/Mobile Documents/com~apple~CloudDocs/Documents/Vanderbilt/Massion_Lab/Projects/CyTOF_ADC_project/Data/RNA Seq/TMA 36 RNA DNA.xlsx", 
+    sheet = "4163-YZ", col_types = c("text", 
+        "text"))
+
+x <- match(paste0('X',gsub('-', '.', p_4163YZ$Vantage_ID)), colnames(rna_4163YZ)[8:ncol(rna_4163YZ)]) + 7
+
+rna_4163YZ <- rna_4163YZ[,c(1:7,x)]
+
+colnames(rna_4163YZ)[8:36] == paste0('X',gsub('-', '.', p_4163YZ$Vantage_ID))
+
+colnames(rna_4163YZ)[8:36] = p_4163YZ$Vantage_ID
+
+p_4163YZ['Batch'] = 2
+
+p_4163YZ <- p_4163YZ[-29,]
+rna_4163YZ[,36] <- NULL
+
+
+# Combining both 
+
+x <- which(rna_3388YZ$Feature %in% rna_4163YZ$Feature)
+x2 <- which(rna_4163YZ$Feature %in% rna_3388YZ$Feature)
+
+rna_3388YZ <- rna_3388YZ[x,]
+rna_4163YZ <- rna_4163YZ[x2,]
+
+rna_3388YZ$Feature <- factor(rna_3388YZ$Feature)
+rna_4163YZ$Feature <- factor(rna_4163YZ$Feature)
+
+table(rna_3388YZ$Feature == rna_4163YZ$Feature) # all should be TRUE
+
+rna_all <- cbind(rna_3388YZ, rna_4163YZ[,8:ncol(rna_4163YZ)])
+p_all <- rbind(p_3388YZ, p_4163YZ)
+
+p_all$Vantage_ID = paste0('R',gsub('-', '_', p_all$Vantage_ID))
+
+colnames(rna_all)[8:ncol(rna_all)] = p_all$Vantage_ID
+
+
+
+
+
