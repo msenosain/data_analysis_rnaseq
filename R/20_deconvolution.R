79cc744070cdcf68f0f2de9805348355a63320da
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-04-09 12:12:16 +0200

    load data

--- R/20_deconvolution.R
+++ R/20_deconvolution.R
@@ -1,3 +1,14 @@
+# Load data
+
+load("/Users/senosam/Documents/Massion_lab/RNASeq_summary/rnaseq.RData")
+
+# write txt files for xcell and cibersort
+write.table(tpm_all_cbs, file = "rnaseq_tpm_cbs.txt", sep = "\t",
+            row.names = FALSE, quote = FALSE)
+write.table(tpm_all, file = "rnaseq_tpm.txt", sep = "\t",
+            row.names = TRUE, quote = FALSE)
+
+
 # Deconvolution
 
 # MCP counter
@@ -12,4 +23,10 @@ write.table(qts_dcv, file = "qts_dcv.txt", sep = "\t",
             row.names = FALSE, quote = FALSE)
 
 
-# CIBERSORT and xCell done online
+# CIBERSORT and xCell deconv done online
+
+# Read in everything
+mcp_dcv <- read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/MCPcounter/mcp_dcv.txt", row.names=1)
+qts_dcv <- read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/quanTIseq/qts_dcv.txt", row.names=1)
+cbs_dcv <- t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/CIBERSORT/CIBERSORT.Output_Job8.txt", row.names=1))
+xcell_dcv <- read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/xCell/xCell_rnaseq_tpm_xCell_0746040120.txt", row.names=1)
