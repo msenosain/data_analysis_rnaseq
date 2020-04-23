3d7a5783b4b9387f3ee98852ef9f7484c3bfbdc4
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-04-23 11:45:53 +0200

    becnhmarking

--- R/21_deconv_benchmarking.R
+++ R/21_deconv_benchmarking.R
@@ -8,6 +8,20 @@ qts_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/decon
 cbs_dcv <- read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/CIBERSORT/CIBERSORT.Output_Job8.txt", row.names=1)
 xcell_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/xCell/xCell_rnaseq_tpm_xCell_0746040120.txt", row.names=1)))
 
+mcp_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/MCPcounter/mcp_fpkm_dcv.txt", row.names=1)))
+qts_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/quanTIseq/qts_fpkm_dcv.txt", row.names=1)))
+cbs_dcv <- read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/CIBERSORT/CIBERSORT.Output_Job9.txt", row.names=1)
+xcell_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/xCell/xCell_rnaseq_fpkm_xCell_1359041620.txt", row.names=1)))
+
+
+mcp_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/MCPcounter/mcp_fpkm_dcv_u.txt", row.names=1)))
+qts_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/quanTIseq/qts_fpkm_dcv_u.txt", row.names=1)))
+cbs_dcv <- read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/CIBERSORT/CIBERSORT.Output_Job10.txt", row.names=1)
+xcell_dcv <- data.frame(t(read.delim("~/Documents/Massion_lab/RNASeq_summary/deconvolution/output/xCell/xCell_rnaseq_fpkm_u_xCell_0432041720.txt", row.names=1)))
+
+
+
+
 # Load RNA seq data 
 load("/Users/senosam/Documents/Massion_lab/RNASeq_summary/rnaseq.RData")
 
@@ -53,6 +67,9 @@ qts_dcv <- qts_dcv[pt_cytof,]
 cbs_dcv <- cbs_dcv[pt_cytof,]
 xcell_dcv <- xcell_dcv[pt_cytof,]
 prcnt_pt <- prcnt_pt[pt_cytof,]
+prcnt_pt['Non_immune'] <- rowSums(prcnt_pt[,1:3])
+prcnt_pt['method'] <- 'cytof'
+
 
 # Load excel cell types names
 mcp_ctnames <- readxl::read_excel("/Users/senosam/Documents/Massion_lab/RNASeq_summary/deconvolution/deconv_ctnames.xlsx", 
@@ -64,34 +81,65 @@ cbs_ctnames <- readxl::read_excel("/Users/senosam/Documents/Massion_lab/RNASeq_s
 xcell_ctnames <- readxl::read_excel("/Users/senosam/Documents/Massion_lab/RNASeq_summary/deconvolution/deconv_ctnames.xlsx", 
     sheet = "xcell")
 
-mcp_ctnames[]<- lapply(mcp_ctnames, function(x) gsub('\\+|\\-|\\s', '.', x))
-qts_ctnames[]<- lapply(qts_ctnames, function(x) gsub('\\+|\\-|\\s', '.', x))
-cbs_ctnames[]<- lapply(cbs_ctnames, function(x) gsub('\\+|\\-|\\s', '.', x))
-xcell_ctnames[]<- lapply(xcell_ctnames, function(x) gsub('\\+|\\-|\\s', '.', x))
+mcp_ctnames[]<- lapply(mcp_ctnames, function(x) gsub('\\+|\\-|\\s|\\(|\\)', '.', x))
+qts_ctnames[]<- lapply(qts_ctnames, function(x) gsub('\\+|\\-|\\s|\\(|\\)', '.', x))
+cbs_ctnames[]<- lapply(cbs_ctnames, function(x) gsub('\\+|\\-|\\s|\\(|\\)', '.', x))
+xcell_ctnames[]<- lapply(xcell_ctnames, function(x) gsub('\\+|\\-|\\s|\\(|\\)', '.', x))
 
 # match names
-mcp_ctnames_v <- as.vector(as.matrix(mcp_ctnames))
-mcp_ctnames_v <- mcp_ctnames_v[!is.na(mcp_ctnames_v)]
-
-mcp_ctnames_v <- as.vector(as.matrix(mcp_ctnames))
-mcp_ctnames_v <- mcp_ctnames_v[!is.na(mcp_ctnames_v)]
-
-mcp_ctnames_v <- as.vector(as.matrix(mcp_ctnames))
-mcp_ctnames_v <- mcp_ctnames_v[!is.na(mcp_ctnames_v)]
-
-mcp_ctnames_v <- as.vector(as.matrix(mcp_ctnames))
-mcp_ctnames_v <- mcp_ctnames_v[!is.na(mcp_ctnames_v)]
-
-
-somefunction <- function(ctnames_df, deconv_results){
+rearrange_dcv <- function(ctnames_df, deconv_results, method_name){
     ctnames_v <- as.vector(as.matrix(ctnames_df))
     ctnames_v <- ctnames_v[!is.na(ctnames_v)]
 
     deconv_results <- deconv_results[,ctnames_v]
 
+    new_dcv <- data.frame(matrix(0, 
+        nrow = nrow(deconv_results), 
+        ncol = ncol(ctnames_df), 
+        dimnames = list(rownames(deconv_results), colnames(ctnames_df))))
+
+    for(i in colnames(new_dcv)){
+        v <- as.vector(as.matrix(ctnames_df[,i]))
+        v <- v[!is.na(v)]
+        if (length(v)==1){
+            new_dcv[,i] <- deconv_results[,v]
+        } else if (length(v)>1) {
+            new_dcv[,i] <- rowSums(deconv_results[,v])
+        } else if(length(v)==0) {
+            new_dcv[,i] = NA
+        }
+    }
+    new_dcv['method'] <- method_name
+    new_dcv
 }
 
+mcp_new <- rearrange_dcv(mcp_ctnames, mcp_dcv, 'MCP')
+qts_new <- rearrange_dcv(qts_ctnames, qts_dcv, 'QTS')
+cbs_new <- rearrange_dcv(cbs_ctnames, cbs_dcv, 'CBS')
+xcell_new <- rearrange_dcv(xcell_ctnames, xcell_dcv, 'XCL')
 
-rowsum(mcp_dcv[,mcp_ctnames$T_cells])
 
 # plot correlations
+
+
+all_dt <- rbind(mcp_new, qts_new, cbs_new, xcell_new)
+
+all_dt <- reshape::melt(all_dt)
+
+all_dt <- all_dt[order(all_dt$method),]
+
+ref <- reshape::melt(prcnt_pt)
+
+ref <- rbind(ref,ref,ref,ref)
+
+all_dt <- cbind(all_dt, 'ref'= ref$value)
+
+all_dt_1 <- all_dt[which(all_dt$batch =='1'),]
+all_dt_2 <- all_dt[which(all_dt$batch =='2'),]
+
+ggpubr::ggscatter(all_dt_2, x = "ref", y = "value",
+          add = "reg.line", conf.int = F, combine = TRUE,
+          cor.coef = TRUE, cor.method = "spearman", add.params = list(color = 'blue'),
+          xlab = "CyTOF fraction", ylab = 'Estimated fraction') +
+    ggplot2::facet_grid(vars(method), vars(variable), scales = "free")
+
